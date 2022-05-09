bd249356b251976cad84dbb7fd56ae1f00a6139a
xu <602344037@qq.com>
2022-05-09 19:44:56 +0800

    test antui

---
+++ uitester/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CollaspList/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CollaspList from './components/CollaspList'
+
+export default {
+  name: 'App',
+  components: {
+    CollaspList,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ uitester/src/components/CollaspList.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-collapse v-model:activeKey="activeKey" ghost>
+    <a-collapse-panel key="1" header="This is panel header 1">
+      <p>{{ text }}</p>
+    </a-collapse-panel>
+    <a-collapse-panel key="2" header="This is panel header 2">
+      <p>{{ text }}</p>
+    </a-collapse-panel>
+    <a-collapse-panel key="3" header="This is panel header 3" collapsible="disabled">
+      <p>{{ text }}</p>
+    </a-collapse-panel>
+  </a-collapse>
+  <a-radio-group v-model:value="size">
+    <a-radio-button value="large">Large</a-radio-button>
+    <a-radio-button value="default">Default</a-radio-button>
+    <a-radio-button value="small">Small</a-radio-button>
+  </a-radio-group>
+</template>
+<script>
+import { defineComponent, ref, watch } from 'vue';
+export default defineComponent({
+  setup() {
+    const text = `A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.`;
+    const activeKey = ref(['1']);
+    watch(activeKey, val => {
+      console.log(val);
+    });
+    return {
+      text,
+      activeKey,
+    };
+  },
+
+});
+</script>
